4be502c15ecd083c25a8c13ef6f9bf2f699dfbfb
ArsenAjiev <selftcoder@gmail.com>
2022-01-17 15:21:16 +0300

    add parametrize

---
+++ tests/fixtures/test_03.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+@pytest.fixture
+def data_1():
+    return 1
+
+
+@pytest.fixture
+def print_hello():
+    print('\nHello')
+
+
+def test_data_1(data_1, print_hello):
+    assert data_1 == 1
+
+
+# def test_data_2(data_2):
+#     assert data_2 == 2
